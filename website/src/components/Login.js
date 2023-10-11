ec751539f177e4832d9433be36e9f291bc353320
AllenF04 <143034745+AllenF04@users.noreply.github.com>
2023-10-11 21:26:53 +0000

    Working with Axios

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import axios from 'axios'
+
 function Login() {
+  const [post, setPost] = useState({
+    user:'',
+    pwd:''
+  });
+
+  const handleInput = (event) => {
+    setPost({user:event.target.value, pwd:event.target.value});
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post("https://reqres.in/api/users", {post})
+    .then((response) => console.log(response))
+  }
   return (
     <div className="login-container">
         <h2>SIGN INTO YOUR ACCOUNT</h2>
-        <form>
-            <input className="login-input" type="text" id="user" name="user" placeholder='Your Email'/><br></br>
-            <input className="login-input"type="text" id="pwd" name="pwd" placeholder='Your Password'/><br></br>
-            <input className="login-submit"type="submit" value="Sign in"/>
+        <form onSubmit={handleSubmit}>
+            <input className="login-input" onChange={handleInput} type="text" id="user" name="user" placeholder='Your Email'/><br></br>
+            <input className="login-input" onChange={handleInput} type="text" id="pwd" name="pwd" placeholder='Your Password'/><br></br>
+            <input className="login-submit" type="submit" value="Sign in"/>
         </form>
         <a href="/">Forgot your password?</a>
 
