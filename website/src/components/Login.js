8486932550760f066bb6a01a389e31833968a865
AllenF04 <143034745+AllenF04@users.noreply.github.com>
2023-10-11 21:26:53 +0000

    Working with Axios

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -3,26 +3,27 @@ import './Login.css'
 import axios from 'axios'
 
 function Login() {
-  const [post, setPost] = useState({
+  const [cred, setCred] = useState({
     user:'',
     pwd:''
   });
 
   const handleInput = (event) => {
-    setPost({user:event.target.value, pwd:event.target.value});
+    console.log(event.target.name + " " + event.target.value);
+    setCred({...cred, [event.target.name] : event.target.value});
   }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post("https://reqres.in/api/users", {post})
+    axios.post("https://reqres.in/api/users", {cred})
     .then((response) => console.log(response))
   }
   return (
     <div className="login-container">
         <h2>SIGN INTO YOUR ACCOUNT</h2>
         <form onSubmit={handleSubmit}>
-            <input className="login-input" onChange={handleInput} type="text" id="user" name="user" placeholder='Your Email'/><br></br>
-            <input className="login-input" onChange={handleInput} type="text" id="pwd" name="pwd" placeholder='Your Password'/><br></br>
+            <input className="login-input" onChange={handleInput} type="text" value={cred.user} id="user" name="user" placeholder='Your Email'/><br></br>
+            <input className="login-input" onChange={handleInput} type="text" value={cred.pwd} id="pwd" name="pwd" placeholder='Your Password'/><br></br>
             <input className="login-submit" type="submit" value="Sign in"/>
         </form>
         <a href="/">Forgot your password?</a>
