9305a421137057c918a77b09f7b150c53ebf983b
AllenF04 <143034745+AllenF04@users.noreply.github.com>
2023-11-15 22:40:30 +0000

    Prisma

--- backendPractice/db.js
+++ backendPractice/db.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    host:'localhost',
-    port:5432,
-    user:'myuser',
-    password:'mypassword',
-    database:'mydb'
+    host: 'localhost',
+    port: 5432,
+    user: 'myuser',
+    password: 'mypassword',
+    database: 'mydb'
 })
 module.exports = pool;
