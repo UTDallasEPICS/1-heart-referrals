02d32ff6ade25a8f91d3b1d2b28cf6a22b0ddb71
Shelby Vo <91278362+ncp9988@users.noreply.github.com>
2023-11-27 21:09:01 +0000

    merge before pull

--- website/src/index.js
+++ website/src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './components/context/AuthProvider';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <App />
-// );
 
-// ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-    <React.StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <AuthProvider>
+//         <App />
+//       </AuthProvider>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
